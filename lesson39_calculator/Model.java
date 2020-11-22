ffd7f414bd5979f506859b92e3a7670c73167f75
Babichvm26 <70631894+Babichvm26@users.noreply.github.com>
2020-11-22 17:29:13 +0200

    Add files via upload

---
+++ lesson39_calculator/Model.java
@@ -0,0 +1,19 @@
+package lesson39_calculator;
+
+public class Model {
+    int x;
+    int y;
+    Model(int r, int t){
+        x = r;
+        y = t;
+
+    }
+    Model(){
+        System.out.println("Empty");
+    }
+    Model(int h){ x = h;}
+
+
+    }
+
+
---
+++ lesson39_calculator/View.java
@@ -0,0 +1,15 @@
+package lesson39_calculator;
+
+public class View {
+
+    void displayInt(int result) {
+            System.out.println(result);
+    }
+    void displayDouble(double result) {
+        System.out.println(result);
+
+    }
+    void display  (int result  ){
+        System.out.println(result);
+    }
+}
