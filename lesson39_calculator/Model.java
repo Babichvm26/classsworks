bc51f2fe7ae74e87e42a7503b170c4f7668441c2
Babichvm26 <70631894+Babichvm26@users.noreply.github.com>
2020-11-24 17:41:47 +0200

    Add files via upload

---
+++ lesson39_calculator/Model.java
@@ -0,0 +1,11 @@
+package lesson39_calculator;
+
+public class Model {
+    int x;
+    int y;
+    double result;
+
+
+    }
+
+
---
+++ lesson39_calculator/View.java
@@ -0,0 +1,14 @@
+package lesson39_calculator;
+
+public class View {
+
+    void displayInt (int result){
+        System.out.println(result);
+
+    }
+    void displayDouble (double result){
+        System.out.println(result);
+
+    }
+
+}
