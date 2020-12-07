5885a094dbc6275077ef5e9385e4d3bbb4755e45
Babichvm26 <70631894+Babichvm26@users.noreply.github.com>
2020-12-07 12:51:00 +0200

    Add files via upload

---
+++ lesson39_calculator/Controller.java
@@ -0,0 +1,17 @@
+package lesson39_calculator;
+
+public class Controller {
+
+     int add(int x, int y){ return x+y; }
+
+     int substract(int x, int y){
+        return x-y;
+    }
+
+     int multiply(int x, int y){
+        return x*y;
+    }
+
+     double div(int x, int y){ return (double)x/y;}
+}
+
---
+++ lesson39_calculator/Model.java
@@ -0,0 +1,13 @@
+package lesson39_calculator;
+
+public class Model {
+    int x;
+    int y;
+    double result;
+
+    Model(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+}
