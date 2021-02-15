63958db66714c218a1c45086e1d284456edda533
Babichvm26 <70631894+Babichvm26@users.noreply.github.com>
2021-02-15 12:41:55 +0200

    Add files via upload

---
+++ Example_1_1/Main.java
@@ -0,0 +1,28 @@
+package Example_1_1;
+import java.util.Scanner;
+public class Main {
+    //String userInput = "12345";
+
+    public static void main(String[] args) {
+
+
+        String userInput = "12345";
+        Scanner s = new Scanner(System.in);
+        userInput = (String) s.nextLine();
+
+        if (userInput.equals("2")) {
+            return this.name;
+        }
+        if (userInput.equals("2")) {
+            return this.weight;
+        }
+    }
+
+
+
+
+
+
+    }
+
+}
