1981d5e86ec5ffd4e470510f825c91851f4db2c4
Babichvm26 <70631894+Babichvm26@users.noreply.github.com>
2021-05-19 09:58:58 +0100

    Add files via upload

---
+++ lesson_128_1/Date.java
@@ -0,0 +1,42 @@
+package lesson_128_1;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import  java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class Date {
+    public static void main(String[] args) {
+
+//        LocalDate today = LocalDate.now();
+//
+//        //Получить год, проверить високосный ли он
+//
+//        System.out.println(/*...*/);
+//
+//        //Проверить LocalDate: до или после
+//        System.out.println(/*...*/);
+//        //Создать LocalDate:до или после
+//        System.out.println(/*...*/);
+//
+//        //Создать LocalDateTime из LocalDate
+//        System.out.println("Текущее время : "/*+ ...*/);
+//
+//        //Операции + и - с датами
+//
+//        System.out.println("9 дней после сегодняшнего дня: "/*+...*/);
+//        System.out.println("3 недель после сегодняшнего дня: " /*+...*/);
+//        System.out.println("20 месяцев после сегодняшнего дня: "/*+...*/);
+
+        //Вывести...
+//        System.out.println("Первый день этого месяца:" /*+...*/);
+//        LocalDate lastDayOfYear = today.with(TemporalAdjusters.lastDayOfYear());
+//        System.out.println("Последний день этого года:"/*+...*/);
+
+//        Period period = today.until(lastDayOfYear());
+//        System.out.println("Найти время между двумя датами :" /*+...*/);
+//        System.out.println("В этом году осталось "/*+...+*/"месяца(ев)");
+
+
+   }
+}
